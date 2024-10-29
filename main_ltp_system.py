d58968551db4a9f1266c5bc8c40db616aff7bd5d
matildevalente <93744917+matildevalente@users.noreply.github.com>
2024-10-29 20:23:20 +0100

    final main files

--- main_ltp_system.py
+++ main_ltp_system.py
@@ -11,8 +11,8 @@ from tqdm import tqdm
 import torch.nn as nn
 from typing import Dict, Any, Tuple
 
-from src.ltp_system.utils import set_seed, load_dataset, load_config
-from src.ltp_system.data_prep import DataPreprocessor
+from src.ltp_system.utils import set_seed, load_dataset, load_config, select_random_rows, split_dataset
+from src.ltp_system.data_prep import DataPreprocessor, setup_dataset_with_preproprocessing_info
 from src.ltp_system.plotter.eda import apply_eda
 from src.ltp_system.pinn_nn import get_trained_bootstraped_models, load_checkpoints, NeuralNetwork
 from src.ltp_system.projection import get_inverse_covariance_matrix , print_matrix, compute_projection_results, compute_mape_physical_laws, compute_mape_physical_laws_loki
@@ -20,7 +20,7 @@ from src.ltp_system.plotter.loss_curves import loss_curves
 from src.ltp_system.plotter.barplots import Figure_4d, Figure_4a, Figure_4b
 from src.ltp_system.plotter.Figure_6a import Figure_6a, run_experiment_6a
 from src.ltp_system.plotter.Figure_6e import Figure_6e, run_experiment_6e
-from src.ltp_system.plotter.outputs_vs_pressure import get_data_Figure_6b, Figure_6b
+from src.ltp_system.plotter.outputs_vs_pressure import get_data_Figure_6b, Figure_6b, run_fig_6b_experiments
 
 
 
@@ -36,96 +36,104 @@ def main():
         # Load the configuration file
         config = load_config('configs/ltp_system_config.yaml')
 
-        # /// 1. EXTRACT DATASET ///
-        df_dataset, full_dataset = load_dataset(config, dataset_dir = 'data/ltp_system/data_1000_points.txt')
+        # /// 1. EXTRACT FULL DATASET ///
+        large_dataset_path = 'data/ltp_system/data_3000_points.txt'
+        _, large_dataset = load_dataset(config, large_dataset_path)
         
-        # /// 2. PREPROCESS THE DATASET ///
-        preprocessed_data = DataPreprocessor(config)
-        preprocessed_data.setup_dataset(full_dataset.x, full_dataset.y)        
-        #apply_eda(config, preprocessed_data, full_dataset.y)
-
+        # /// 2. PREPROCESS THE FULL DATASET ///
+        data_preprocessing_info = DataPreprocessor(config)
+        data_preprocessing_info.setup_dataset(large_dataset.x, large_dataset.y) 
+        testing_file, training_file = split_dataset(large_dataset_path, n_testing_points = 300)     
+        #apply_eda(config, data_preprocessing_info, large_dataset.y)
+        
         # /// 3. TRAIN THE NEURAL NETWORK (NN) ///
-        nn_models, nn_losses_dict, device_nn = get_trained_nn(config, preprocessed_data)
-        loss_curves(config['nn_model'], config['plotting'], nn_losses_dict)
+        nn_models, nn_losses_dict, device_nn = get_trained_nn(config, data_preprocessing_info, training_file, dataset_size = 1000)
+        #loss_curves(config['nn_model'], config['plotting'], nn_losses_dict)
     
         # /// 4. TRAIN THE PHYSICS-INFORMED NEURAL NETWORK (PINN) ///
-        pinn_models, pinn_losses_dict, device_pinn = get_trained_pinn(config, preprocessed_data)
-        loss_curves(config['pinn_model'], config['plotting'], pinn_losses_dict)
+        pinn_models, pinn_losses_dict, device_pinn = get_trained_pinn(config, data_preprocessing_info, training_file, dataset_size = 1000)
+        #loss_curves(config['pinn_model'], config['plotting'], pinn_losses_dict)
 
-        
-        # /// 5. GET MATRIX TO PERFORM PROJECTION ///
-        w_inv_cov = get_inverse_covariance_matrix(config, preprocessed_data, nn_models, device_nn)
-        print_matrix(w_inv_cov)
-
-        # /// 6. BARPLOTS COMPARING THE ERROR WHEN APPLYING DIFFERENT SETS OF CONSTRAINTS /// 
-        file_path = 'data/ltp_system/data_300_points.txt'
-        nn_mape_sem_dict = compute_projection_results(config['nn_model'], torch.eye(17), file_path, preprocessed_data, nn_models)
-        pinn_mape_sem_dict = compute_projection_results(config['pinn_model'], torch.eye(17), file_path, preprocessed_data, pinn_models)
+        # /// 5. BARPLOTS COMPARING THE ERROR WHEN APPLYING DIFFERENT SETS OF CONSTRAINTS /// 
+        nn_mape_sem_dict = compute_projection_results(config['nn_model'], torch.eye(17), testing_file, data_preprocessing_info, nn_models)
+        pinn_mape_sem_dict = compute_projection_results(config['pinn_model'], torch.eye(17), testing_file, data_preprocessing_info, pinn_models)
         Figure_4a(config['plotting'], nn_mape_sem_dict, pinn_mape_sem_dict)
         Figure_4d(nn_mape_sem_dict, config['nn_model'], config['plotting'])  
 
-        # /// 7. BARPLOTS COMPARING THE ERROR IN COMPLIANCE WITH PHYSICAL LAWS /// 
-        laws_dict = get_laws_dict(file_path, preprocessed_data, nn_models, pinn_models)
+        # /// 6. BARPLOTS COMPARING THE ERROR IN COMPLIANCE WITH PHYSICAL LAWS /// 
+        laws_dict = get_laws_dict(testing_file, data_preprocessing_info, nn_models, pinn_models)
         Figure_4b(config['plotting'], laws_dict)
-    
-        # /// 8. CONST CURRENT PLOTS ///
-        #file_path = 'data/ltp_system/const_current/data_50_points_30mA.txt'
-        #preds_dict, errors_dict = get_data_Figure_6b(config, nn_models, file_path, torch.eye(17))
-        #Figure_6b(config, preds_dict, errors_dict)
+        
+        # /// 7. NN and NN_Proj errors as a func of the model's parameters + NN training time /// 
+        """options = {
+            'n_bootstrap_models': 30,
+            'activation_func': 'leaky_relu', 
+            'APPLY_EARLY_STOPPING': False,
+            'num_epochs': 50,
+            'max_hidden_layers': 1, 
+            'min_hidden_layers':1,
+            'max_neurons_per_layer': 500, 
+            'min_neurons_per_layer': 1, 
+            'n_steps': 35,
+            'w_matrix': torch.eye(17), 
+            'PRINT_LOSS_VALUES': False,
+            'RETRAIN_MODEL': False
+        }
+        temp_file = select_random_rows(training_file, 600)
+        df_results_6a = run_experiment_6a(config, temp_file, options)
+        Figure_6a(config['plotting'], df_results_6a)"""
 
-        # /// 9. NN and NN_Proj errors as a func of the dataset size + approximate loki computation time /// 
+        # /// 8. NN and NN_Proj errors as a func of the dataset size + approximate loki computation time /// 
         options = {
-            'n_bootstrap_models': 10, 
+            'n_samples': 30, 
+            'APPLY_EARLY_STOPPING': False,
+            'num_epochs':  10,
+            'n_bootstrap_models': 5, 
             'hidden_sizes': [451, 315, 498, 262],
-            'num_epochs':  100,
             'activation_fns': ['leaky_relu', 'leaky_relu', 'leaky_relu', 'leaky_relu'], 
             'w_matrix': torch.eye(17), 
             'PRINT_LOSS_VALUES': False,
             'RETRAIN_MODEL': False, 
         }
         # Get the list of files to analyze
-        dataset_sizes = [175, 200, 250, 300, 350, 400, 500, 600, 700, 800, 900, 1000, 1500, 2000, 2500, 3000]
-        large_dataset = 'data/ltp_system/data_15000_points.txt'
-        df_results_6e = run_experiment_6e(config, large_dataset, dataset_sizes, options)
+        dataset_sizes = [175, 200, 250, 300, 350, 400, 500, 600, 700, 800, 900, 1000]
+        df_results_6e = run_experiment_6e(config, training_file, dataset_sizes, options)
+        print(df_results_6e)
         Figure_6e(config['plotting'], df_results_6e)
-
-        # /// 10. NN and NN_Proj errors as a func of the model's parameters + NN training time /// 
-        """options = {
-            'n_bootstrap_models':  10,
-            'activation_func': 'leaky_relu', 
-            'max_hidden_layers': 1, 
-            'min_hidden_layers':1,
-            'max_neurons_per_layer': 75, 
-            'min_neurons_per_layer': 1, 
-            'n_steps': 20,
+        
+        """# /// 9. CONST CURRENT PLOTS ///
+        dataset_sizes = [1000]
+        options = {
+            'num_epochs':  100,
+            'APPLY_EARLY_STOPPING': False,
+            'n_bootstrap_models': 30, 
+            'hidden_sizes': [451, 315, 498, 262],
+            'activation_fns': ['leaky_relu', 'leaky_relu', 'leaky_relu', 'leaky_relu'],
             'w_matrix': torch.eye(17), 
-            'num_epochs': 50,
-            'PRINT_LOSS_VALUES': False,
-            'RETRAIN_MODEL': False
+            'RETRAIN_MODEL': True, 
+            'PRINT_LOSS_VALUES': False
         }
-        file_path = 'data/ltp_system/data_600_points.txt'
-        df_results_6a = run_experiment_6a(config, file_path, options)
-        Figure_6a(config['plotting'], df_results_6a)"""
+        training_file = 'data/ltp_system/data_1000_points.txt'
+        target_data_file_path = 'data/ltp_system/const_current/data_50_points_30mA.txt'"""
+        #run_fig_6b_experiments(config, options, dataset_sizes, target_data_file_path, training_file, testcase = "a")
+        
+        options['num_epochs'] = 50
+        options['n_bootstrap_models'] = 1
+        #run_fig_6b_experiments(config, options, dataset_sizes, target_data_file_path, training_file, testcase = "b")
+
+        options['num_epochs'] = 5
+        options['n_bootstrap_models'] = 1
+        #run_fig_6b_experiments(config, options, dataset_sizes, target_data_file_path, training_file, testcase = "c")
+
+        dataset_sizes = [300]
+        options['num_epochs'] = 5
+        options['n_bootstrap_models'] = 1
+        training_file = 'data/ltp_system/data_1000_points.txt'
+        #run_fig_6b_experiments(config, options, dataset_sizes, target_data_file_path, training_file, testcase = "d")
 
 
 
 
-        """# /// 6. EVALUATE ONE INITIAL CONDITION (Fig. 2) ///
-        initial_state = [ 0.23334019,  0.45744362, -0.07293496, -0.43316936] #[0.37987354, 0.19292271, 0.43858615, 0.82498616]##0.9396753206665032,2.6248020727512547,0.679625428401559,-1.5368544850961408
-        n_time_steps = 50
-        df_target = get_target_trajectory(config, n_time_steps = n_time_steps, initial_state = torch.tensor(initial_state))
-        df_nn   = get_predicted_trajectory(config, preprocessed_data, nn_model,   n_time_steps = n_time_steps, initial_state = torch.tensor(initial_state))
-        df_pinn = get_predicted_trajectory(config, preprocessed_data, pinn_model, n_time_steps = n_time_steps, initial_state = torch.tensor(initial_state))
-        df_proj_nn   = get_projection_df(initial_state, n_time_steps, nn_model, torch.eye(4), preprocessed_data, config, df_nn)
-        df_proj_pinn = get_projection_df(initial_state, n_time_steps, pinn_model, torch.eye(4), preprocessed_data, config, df_pinn)
-
-        # Make plots
-        plot_predicted_energies_vs_target(config, df_target, df_nn, df_pinn, df_proj_nn, df_proj_pinn)
-        plot_predicted_trajectory_vs_target(config, df_target, df_nn, df_pinn, df_proj_nn, df_proj_pinn)
-        plot_bar_plot(config, df_target, df_nn, df_pinn, df_proj_nn, df_proj_pinn, preprocessed_data)
-
-        # /// 6. EVALUATE SEVERAL INITIAL CONDITIONS (Fig. 3 & Table 1) ///test_initial_conditions
-        plot_several_initial_conditions(config, preprocessed_data, nn_model, pinn_model, test_initial_conditions, w_inv_cov, n_time_steps = n_time_steps)"""
 
 
     except Exception as e:
@@ -133,18 +141,25 @@ def main():
         raise
 
 
-def get_trained_nn(config: Dict[str, Any], preprocessed_data: Any) -> Tuple[nn.Module, Tuple[list, list]]:
+def get_trained_nn(config, data_preprocessing_info, training_file, dataset_size):
+    # create checkpoints directory for the trained models
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     checkpoint_dir = os.path.join('output', 'ltp_system', 'checkpoints', 'nn')
     os.makedirs(checkpoint_dir, exist_ok=True)
-    checkpoint_path = os.path.join(checkpoint_dir, 'latest_checkpoint.pth')
     
-    train_data = preprocessed_data.train_data
-    val_loader = torch.utils.data.DataLoader(preprocessed_data.val_data, batch_size=config['nn_model']['batch_size'], shuffle=True)
-    
+    # read from the train_path file and randomly select 'dataset_size' rows
+    temp_file = select_random_rows(training_file, dataset_size)
+
+    # read the dataset and preprocess data
+    _, temp_dataset = load_dataset(config, temp_file)
+    train_data, _, val_data  = setup_dataset_with_preproprocessing_info(temp_dataset.x, temp_dataset.y, data_preprocessing_info)  
+
+    # create the val loader
+    val_loader = torch.utils.data.DataLoader(val_data, batch_size=config['nn_model']['batch_size'], shuffle=True)
+
     if config['nn_model']['RETRAIN_MODEL']:
 
-        nn_models, nn_losses_dict, _ = get_trained_bootstraped_models(config['nn_model'], config['plotting'], preprocessed_data, nn.MSELoss(), checkpoint_dir, device, val_loader, train_data)
+        nn_models, nn_losses_dict, _ = get_trained_bootstraped_models(config['nn_model'], config['plotting'], data_preprocessing_info, nn.MSELoss(), checkpoint_dir, device, val_loader, train_data, seed = 'default')
         return nn_models, nn_losses_dict, device
     else:
         try:
@@ -155,7 +170,7 @@ def get_trained_nn(config: Dict[str, Any], preprocessed_data: Any) -> Tuple[nn.M
                 nn_losses_dict['losses_val_mean'].size == 0 or 
                 nn_losses_dict['epoch'].size == 0):
                 print("Warning: Incomplete loss history found in checkpoint. Retraining the model.")
-                nn_models, nn_losses_dict, _ = get_trained_bootstraped_models(config['nn_model'], config['plotting'], preprocessed_data, nn.MSELoss(), checkpoint_dir, device, val_loader, train_data)
+                nn_models, nn_losses_dict, _ = get_trained_bootstraped_models(config['nn_model'], config['plotting'], data_preprocessing_info, nn.MSELoss(), checkpoint_dir, device, val_loader, train_data, seed = 'default')
             else:
                 print("Checkpoint loaded successfully with loss history.")
 
@@ -165,17 +180,25 @@ def get_trained_nn(config: Dict[str, Any], preprocessed_data: Any) -> Tuple[nn.M
     return nn_models, nn_losses_dict, device
 
 
-def get_trained_pinn(config: Dict[str, Any], preprocessed_data: Any) -> Tuple[nn.Module, Tuple[list, list]]:
+def get_trained_pinn(config, data_preprocessing_info, training_file, dataset_size):
+    # create checkpoints directory for the trained models
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     checkpoint_dir = os.path.join('output', 'ltp_system', 'checkpoints', 'pinn')
     os.makedirs(checkpoint_dir, exist_ok=True)
-    checkpoint_path = os.path.join(checkpoint_dir, 'latest_checkpoint.pth')
-    train_data = preprocessed_data.train_data
-    val_loader = torch.utils.data.DataLoader(preprocessed_data.val_data, batch_size=config['pinn_model']['batch_size'], shuffle=True)
     
+    # read from the train_path file and randomly select 'dataset_size' rows
+    temp_file = select_random_rows(training_file, dataset_size)
+
+    # read the dataset and preprocess data
+    _, temp_dataset = load_dataset(config, temp_file)
+    train_data, _, val_data  = setup_dataset_with_preproprocessing_info(temp_dataset.x, temp_dataset.y, data_preprocessing_info)  
+
+    # create the val loader
+    val_loader = torch.utils.data.DataLoader(val_data, batch_size=config['nn_model']['batch_size'], shuffle=True)
+
     if config['pinn_model']['RETRAIN_MODEL']:
 
-        pinn_models, pinn_losses_dict, _ = get_trained_bootstraped_models(config['pinn_model'], config['plotting'], preprocessed_data, nn.MSELoss(), checkpoint_dir, device, val_loader, train_data)
+        pinn_models, pinn_losses_dict, _ = get_trained_bootstraped_models(config['pinn_model'], config['plotting'], data_preprocessing_info, nn.MSELoss(), checkpoint_dir, device, val_loader, train_data, seed = 'default')
         return pinn_models, pinn_losses_dict, device
     else:
         try:
@@ -186,7 +209,7 @@ def get_trained_pinn(config: Dict[str, Any], preprocessed_data: Any) -> Tuple[nn
                 pinn_losses_dict['losses_val_mean'].size == 0 or 
                 pinn_losses_dict['epoch'].size == 0):
                 print("Warning: Incomplete loss history found in checkpoint. Retraining the model.")
-                pinn_models, pinn_losses_dict, _ = get_trained_bootstraped_models(config['pinn_model'], config['plotting'], preprocessed_data, nn.MSELoss(), checkpoint_dir, device, val_loader, train_data)
+                pinn_models, pinn_losses_dict, _ = get_trained_bootstraped_models(config['pinn_model'], config['plotting'], data_preprocessing_info, nn.MSELoss(), checkpoint_dir, device, val_loader, train_data, seed = 'default')
             else:
                 print("Checkpoint loaded successfully with loss history.")     
         
--- main_spring_mass_system.py
+++ main_spring_mass_system.py
@@ -29,7 +29,6 @@ def load_config(config_path):
     with open(config_path, 'r') as f:
         return yaml.safe_load(f)
 
-
 # Load the dataset from a .csv file or generate the entire dataset
 def load_or_generate_dataset(config):
     # Define column names
@@ -47,6 +46,69 @@ def load_or_generate_dataset(config):
     else:
         raise ValueError("Invalid value for GENERATE_DATASET in config. Must be True or False.")
 
+# 
+def get_trained_nn(config: Dict[str, Any], preprocessed_data: Any) -> Tuple[nn.Module, Tuple[list, list]]:
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    nn_model = NeuralNetwork(config).to(device)
+    loss_fn = nn.MSELoss()
+    learning_rate = config['nn_model']['learning_rate']
+    optimizer = torch.optim.Adam(nn_model.parameters(), lr=learning_rate)
+
+    checkpoint_dir = os.path.join('output', 'spring_mass_system', 'checkpoints', 'nn')
+    os.makedirs(checkpoint_dir, exist_ok=True)
+    checkpoint_path = os.path.join(checkpoint_dir, 'latest_checkpoint.pth')
+
+    if config['nn_model']['RETRAIN_MODEL']:
+        train_losses, val_losses = train_nn(config, nn_model, preprocessed_data, loss_fn, optimizer, device, checkpoint_dir)
+        return nn_model, (train_losses, val_losses), device
+    else:
+        try:
+            nn_model, optimizer, _, losses = load_checkpoint(nn_model, optimizer, checkpoint_path)
+        except FileNotFoundError:
+            raise ValueError("Checkpoint not found. Set RETRAIN_MODEL to True or provide a valid checkpoint.")
+        
+        if not losses['train_losses'] or not losses['val_losses']:
+            print("Warning: Loss history not found in checkpoint. Returning empty lists for losses.")
+            train_losses, val_losses = [], []
+
+        return nn_model, (losses['train_losses'], losses['val_losses']), device
+
+#   
+def get_trained_pinn(config: Dict[str, Any], preprocessed_data: Any, checkpoint_dir) -> tuple:
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    
+    pinn_model = PhysicsInformedNN(config).to(device)
+    loss_fn = nn.MSELoss()
+    learning_rate = config['pinn_model']['learning_rate']
+    optimizer = torch.optim.Adam(pinn_model.parameters(), lr=learning_rate)
+    
+    os.makedirs(checkpoint_dir, exist_ok=True)
+    checkpoint_path = os.path.join(checkpoint_dir, 'latest_checkpoint.pth')
+    
+    if config['pinn_model']['RETRAIN_MODEL']:
+        losses = train_pinn(config, pinn_model, preprocessed_data, loss_fn, optimizer, device, checkpoint_dir)
+    else:
+        try:
+           pinn_model, optimizer, _, losses = load_checkpoint(pinn_model, optimizer, checkpoint_path) 
+        except FileNotFoundError:
+            raise ValueError("Checkpoint not found. Set RETRAIN_MODEL to True or provide a valid checkpoint.")
+        
+        if not losses['train_losses'] or not losses['val_losses']:
+            print("Warning: Loss history not found in checkpoint. Returning empty lists for losses.")
+
+    return pinn_model, losses
+
+# Plot the loss curves for the NN and PINN models
+def plot_loss_curves(config: Dict[str, Any], nn_losses: tuple, pinn_losses: Dict[str, Any]):
+    plot_loss_curves_nn(config, *nn_losses)
+    plot_loss_curves_pinn(config, pinn_losses)
+
+# retrieves initial conditions from the test loader
+def get_inputs_from_loader(loader):
+    x_test = np.concatenate([X.cpu().numpy() for X, *_ in loader])
+
+    return x_test
 
 
 def main():
@@ -75,122 +137,35 @@ def main():
         # /// 4. TRAIN THE PHYSICS-INFORMED NEURAL NETWORK (PINN) ///
         set_seed(42)
         pinn_model, pinn_losses = get_trained_pinn(config, preprocessed_data, checkpoint_dir=os.path.join('output', 'spring_mass_system', 'checkpoints', 'pinn'))
-        if(config['pinn_model']['OPTIMIZE_ARC']):
-            pinn_model_opt, pinn_opt_losses, config_pinn_opt = optimize_pinn_architecture(config, preprocessed_data)
-        else:
-            config_pinn_opt = load_config('configs/pinn_opt_config.yaml')
-            #pinn_model_opt, pinn_opt_losses = get_trained_pinn(config_pinn_opt, preprocessed_data, checkpoint_dir=os.path.join('output', 'spring_mass_system', 'checkpoints', 'pinn_opt'))
-
-        # Plot loss curves if specified in config
+        
+        # /// 5. PLOT LOSS CURVES FOR THE NN AND PINN ///
         plot_loss_curves(config, nn_losses, pinn_losses)
-
-        # /// 5. GET MATRIX TO PERFORM PROJECTION ///
-        #w_inv_cov_train,  w_inv_cov_trajectory = get_inverse_covariance_matrix(config,nn_model, device_nn, preprocessed_data)
-        #print(w_inv_cov_train)
 
         # /// 6. EVALUATE ONE INITIAL CONDITION (Fig. 2) ///
         initial_state = [-0.15669316, -2.1848829,   0.09043302, -0.16476968]
         n_time_steps  = 165
-        df_target = get_target_trajectory(config, n_time_steps = n_time_steps, initial_state = torch.tensor(initial_state))
-        df_nn   = get_predicted_trajectory(config, preprocessed_data, nn_model,   n_time_steps = n_time_steps, initial_state = torch.tensor(initial_state))
-        df_pinn = get_predicted_trajectory(config, preprocessed_data, pinn_model, n_time_steps = n_time_steps, initial_state = torch.tensor(initial_state))
-        df_proj_nn   = get_projection_df(initial_state, n_time_steps, nn_model, torch.eye(4), preprocessed_data, config, df_nn)
-        df_proj_pinn = get_projection_df(initial_state, n_time_steps, pinn_model, torch.eye(4), preprocessed_data, config, df_pinn)
+        #df_target = get_target_trajectory(config, n_time_steps = n_time_steps, initial_state = torch.tensor(initial_state))
+        #df_nn   = get_predicted_trajectory(config, preprocessed_data, nn_model,   n_time_steps = n_time_steps, initial_state = torch.tensor(initial_state))
+        #df_pinn = get_predicted_trajectory(config, preprocessed_data, pinn_model, n_time_steps = n_time_steps, initial_state = torch.tensor(initial_state))
+        #df_proj_nn   = get_projection_df(initial_state, n_time_steps, nn_model, torch.eye(4), preprocessed_data, config, df_nn)
+        #df_proj_pinn = get_projection_df(initial_state, n_time_steps, pinn_model, torch.eye(4), preprocessed_data, config, df_pinn)
 
         # Make plots
         n_time_steps = 200
-        plot_predicted_trajectory_vs_target(config, df_target, df_nn, df_pinn, df_proj_nn, df_proj_pinn)
-        plot_predicted_energies_vs_target(config, df_target, df_nn, df_pinn, df_proj_nn, df_proj_pinn)
-        plot_bar_plot(config, df_target, df_nn, df_pinn, df_proj_nn, df_proj_pinn, preprocessed_data)
+        #plot_predicted_trajectory_vs_target(config, df_target, df_nn, df_pinn, df_proj_nn, df_proj_pinn)
+        #plot_predicted_energies_vs_target(config, df_target, df_nn, df_pinn, df_proj_nn, df_proj_pinn)
+        #plot_bar_plot(config, df_target, df_nn, df_pinn, df_proj_nn, df_proj_pinn, preprocessed_data)
 
-        # /// 6. EVALUATE SEVERAL INITIAL CONDITIONS (Fig. 3 & Table 1) ///test_initial_conditions
-        plot_several_initial_conditions(config, preprocessed_data, nn_model, pinn_model, test_initial_conditions, w_inv_cov_matrix = torch.eye(4), n_time_steps = n_time_steps)
-
+        # /// 7. EVALUATE SEVERAL INITIAL CONDITIONS (Fig. 3 & Table 1 & Table 2) 
+        plot_several_initial_conditions(config, preprocessed_data, nn_model, pinn_model, test_initial_conditions, n_time_steps, N_initial_conditions = 100)
 
     except Exception as e:
         logger.error(f"An error occurred: {str(e)}")
         raise
 
 
-def get_trained_nn(config: Dict[str, Any], preprocessed_data: Any) -> Tuple[nn.Module, Tuple[list, list]]:
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-    nn_model = NeuralNetwork(config).to(device)
-    loss_fn = nn.MSELoss()
-    learning_rate = config['nn_model']['learning_rate']
-    optimizer = torch.optim.Adam(nn_model.parameters(), lr=learning_rate)
-
-    checkpoint_dir = os.path.join('output', 'spring_mass_system', 'checkpoints', 'nn')
-    os.makedirs(checkpoint_dir, exist_ok=True)
-    checkpoint_path = os.path.join(checkpoint_dir, 'latest_checkpoint.pth')
-
-    if config['nn_model']['RETRAIN_MODEL']:
-        train_losses, val_losses = train_nn(config, nn_model, preprocessed_data, loss_fn, optimizer, device, checkpoint_dir)
-        return nn_model, (train_losses, val_losses), device
-    else:
-        try:
-            nn_model, optimizer, _, losses = load_checkpoint(nn_model, optimizer, checkpoint_path)
-        except FileNotFoundError:
-            raise ValueError("Checkpoint not found. Set RETRAIN_MODEL to True or provide a valid checkpoint.")
-        
-        if not losses['train_losses'] or not losses['val_losses']:
-            print("Warning: Loss history not found in checkpoint. Returning empty lists for losses.")
-            train_losses, val_losses = [], []
-
-        return nn_model, (losses['train_losses'], losses['val_losses']), device
-    
-def get_trained_pinn(config: Dict[str, Any], preprocessed_data: Any, checkpoint_dir) -> tuple:
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    
-    pinn_model = PhysicsInformedNN(config).to(device)
-    loss_fn = nn.MSELoss()
-    learning_rate = config['pinn_model']['learning_rate']
-    optimizer = torch.optim.Adam(pinn_model.parameters(), lr=learning_rate)
-    
-    os.makedirs(checkpoint_dir, exist_ok=True)
-    checkpoint_path = os.path.join(checkpoint_dir, 'latest_checkpoint.pth')
-    
-    if config['pinn_model']['RETRAIN_MODEL']:
-        losses = train_pinn(config, pinn_model, preprocessed_data, loss_fn, optimizer, device, checkpoint_dir)
-    else:
-        try:
-           pinn_model, optimizer, _, losses = load_checkpoint(pinn_model, optimizer, checkpoint_path) 
-        except FileNotFoundError:
-            raise ValueError("Checkpoint not found. Set RETRAIN_MODEL to True or provide a valid checkpoint.")
-        
-        if not losses['train_losses'] or not losses['val_losses']:
-            print("Warning: Loss history not found in checkpoint. Returning empty lists for losses.")
-
-    return pinn_model, losses
-
-def plot_loss_curves(config: Dict[str, Any], nn_losses: tuple, pinn_losses: Dict[str, Any]):
-    plot_loss_curves_nn(config, *nn_losses)
-    plot_loss_curves_pinn(config, pinn_losses)
-
-def get_inputs_from_loader(loader):
-    x_test = np.concatenate([X.cpu().numpy() for X, *_ in loader])
-    #print(x_test)
-
-    return x_test
-
-
-"""def evaluate_trajectories(config, nn_model, pinn_model, initial_states_list, n_time_steps, preprocessed_data):
-    for initial_state in initial_states_list:
-        df_target = get_target_trajectory(config, n_time_steps, initial_state)
-        df_nn   = get_predicted_trajectory(config, nn_model,   n_time_steps, initial_state, preprocessed_data)
-        df_pinn = get_predicted_trajectory(config, pinn_model, n_time_steps, initial_state, preprocessed_data)
-        #df_proj = predict_trajectory_proj(config, nn_model, n_time_steps, initial_state, preprocessed_data)
-"""
 
 if __name__ == "__main__":
     main()
-
-
-
-
-
-
-
-
 
 
